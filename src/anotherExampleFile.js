ea02311b852b4dca9c29636ff5113860e6530b1d
Your Name <you@example.com>
2025-01-24 23:36:26 +0000

    docs: add JSDoc comments to codebase

---
+++ src/anotherExampleFile.js
@@ -0,0 +1,8 @@
+/**
+ * This function processes data.
+ * @param {Array} data - The data to process.
+ * @returns {Array} The processed data.
+ */
+function processData(data) {
+    return data.map(item => item * 2);
+}
---
+++ src/exampleFile.js
@@ -0,0 +1,7 @@
+/**
+ * This function does something important.
+ * @returns {string} Returns a result.
+ */
+function importantFunction() {
+    return 'result';
+}
